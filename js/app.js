d2ca2bfdfe8fe5b183b479ee402f1fd632cc7baf
subrata sarker <tccdcc8@gmail.com>
2022-02-17 08:26:51 +0600

    Modify

--- js/app.js
+++ js/app.js
@@ -1,70 +1,70 @@
-// calculate area
+// calculate input value 
 function getValue(inputId) {
     var calculateCost = document.getElementById(inputId);
     var calculateCostTk = parseFloat(calculateCost.value);
-
-
-    if (calculateCostTk > 0) {
+    if (!isNaN(calculateCostTk) && calculateCostTk > 0) {
         return calculateCostTk;
     }
-    else if (calculateCostTk<0){
-        return alert('letter or Negative amount not accept');
+    else if (calculateCostTk < 0) {
+         alert('Please give positive/valid amount.');
     }
-    else {
-        return 0;
+    else if (isNaN(calculateCostTk)) {
+         alert('Letter or word is not supported please give valid amount.');
     }
-
-
-
+    return 0;
 }
 
+// calculter button with eventlistener
 document.getElementById('calculateBtn').addEventListener('click', function () {
-    var totalIncome = document.getElementById('incomeValue');
-    var totalIncomeValue = parseFloat(totalIncome.value);
 
-    if (totalIncomeValue < 0) {
-        return alert('Income amount must be Positive')
+    var totalIncome = getValue('incomeValue');
+
+    // total cost calculate
+    var foodCost = getValue('foodValue');
+    var rentCost = getValue('rentValue');
+    var clothsCost = getValue('clothsValue');
+    var totalCost = foodCost + rentCost + clothsCost;
+
+    // balance after cost
+    if(isNaN(totalIncome)){
+        alert(' please give me valid account')
     }
-    else if (isNaN(totalIncomeValue)) {
-        return alert('String or letter not accepted')
+    else if (totalIncome > totalCost ) {
+        var totalCostBalance = document.getElementById('totalCost');
+        totalCostBalance.innerText = totalCost;
+
+        var afterCostBalance = document.getElementById('afterCost');
+        afterCostBalance.innerText = totalIncome - totalCost;
     }
     else {
-        var foodCost = getValue('foodValue');
-        var rentCost = getValue('rentValue');
-        var clothsCost = getValue('clothsValue');
-
-        var totalCost = foodCost + rentCost + clothsCost;
-        var totalCostView = document.getElementById('totalCost');
-        totalCostView.innerText = totalCost;
-        var afterCostView = document.getElementById('afterCost');
-        afterCostView.innerText = totalIncomeValue - totalCost;
+        return alert('Your Cost is too much  but income is too poor');
     }
+
 })
-
-
 
 
 // saving area
 document.getElementById('saveBtn').addEventListener('click', function () {
-    var afterCostView = parseFloat(document.getElementById('afterCost').innerText);
+    var totalIncometk = getValue('incomeValue');
     var saveParsent = getValue('parsentValue');
 
+    var afterCostBalance = document.getElementById('afterCost');
+    var afterCostBalanceTk = parseFloat(afterCostBalance.innerText);
 
-    if (afterCostView > saveParsent) {
-        var saveTk = (afterCostView * saveParsent) / 100;
+    // save tk by persentese
+    var saveTk = (totalIncometk * saveParsent) / 100;
+    var savedTk = document.getElementById('saved');
 
-        var savedView = document.getElementById('saved');
-        savedView.innerText = saveTk;
-
-        var remainingBalance = afterCostView - saveTk;
-        var remainingView = document.getElementById('ramaining');
-        remainingView.innerText = remainingBalance;
-
+    // saving and remaining
+    if (saveTk <= afterCostBalanceTk) {
+        savedTk.innerText = saveTk;
+        var remainingBalance = afterCostBalanceTk - saveTk;
+        var updateRemainingBalance = document.getElementById('ramaining');
+        updateRemainingBalance.innerText = remainingBalance;
     }
     else {
-        return alert("You must Need sufficient Balance");
+        return alert('saving is not possible your balance is low ')
     }
 
-
-
 })
+
